2ac941556033aa918e8c5a5cd371414688ee7a66
Wes Rupert <ora@outlook.com>
2013-04-12 17:26:58 -0400

    Add common.h

---
+++ hw7/common.h
@@ -0,0 +1,17 @@
+#ifndef HW7/COMMON
+#define HW7/COMMON
+
+#define SMOKERCOUNT 3
+#define TOBACCO 0
+#define PAPER   1
+#define MATCHES 2
+
+#define STR_TOBAC "tobacco"
+#define STR_PAPER "paper"
+#define STR_MATCH "matches"
+#define STR_UNKNO "of an unknown substance"
+
+#define ENOUGH 1
+#define NOTENOUGH -1
+
+#endif
